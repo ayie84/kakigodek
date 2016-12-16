a00bac3bad7c6dd2bad6354bf98c5e2b382fdfef
Ahmad Fitri <ahmadfitri@ump.edu.my>
2016-12-16 22:27:06 +0800

    First Commit

---
+++ resources/views/godekbootstrap.blade.php
@@ -0,0 +1,32 @@
+
+@extends('godekmaster')
+
+@section('header')
+<h1>kakigodek</h1>
+@stop
+
+@section('left-sidebar')
+	<h4>Subheading</h4>
+    <p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
+
+    <h4>Subheading</h4>
+    <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus sit amet fermentum.</p>
+
+    <h4>Subheading</h4>
+    <p>Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
+@stop
+
+@section('right-sidebar')
+	<h4>Subheading</h4>
+    <p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
+
+    <h4>Subheading</h4>
+    <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus sit amet fermentum.</p>
+
+    <h4>Subheading</h4>
+    <p>Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
+@stop
+
+@section('footer')
+<h5 allign="center">Hak Milik Kakigodek</h5>
+@stop
